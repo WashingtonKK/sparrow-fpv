016b51d0c6a5266c446ce37c4e6916aaa80688ba
WashingtonKK <washingtonkigan@gmail.com>
2020-05-16 15:21:18 +0300

    Added radio interface

--- radio/radio.ino
+++ radio/radio.ino
@@ -1,23 +1,24 @@
-int ch1;
-int ch2;
-int ch3;
-int ch4;
+int roll;
+int throttle;
+int yaw;
+int pitch;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  //ch1 = pulseIn(4, HIGH);
-  //ch2 = pulseIn(9, HIGH);
-  //ch3 = pulseIn(13, HIGH);
-  ch4 = pulseIn(12, HIGH);
-  //Serial.print("Roll");
-  //Serial.println(ch1);
-  //Serial.print("pitch");
-  //Serial.println(ch2);
-  //Serial.print("Yaw");
-  //Serial.println(ch3);
-  //Serial.print("throttle");
-  Serial.println(ch4);
+  roll = pulseIn(4, HIGH);
+  throttle = pulseIn(9, HIGH);
+  pitch = pulseIn(13, HIGH);
+  yaw = pulseIn(12, HIGH);
+  Serial.print("Roll---");
+  Serial.println(roll) ;
+  Serial.print("pitch--");
+  Serial.println(pitch);
+  Serial.print("Yaw--");
+  Serial.println(yaw);
+  Serial.print("throttle--");
+  Serial.println(throttle);
+  delay(1000);
 }
